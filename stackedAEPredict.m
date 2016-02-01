f3d87b90fd1c5a5a38211c45d401043541a228df
LIRIS-PC <cuanbonan@gmail.com>
2016-02-01 15:43:59 +0100

    all checked

--- stackedAEPredict.m
+++ stackedAEPredict.m
@@ -1,4 +1,4 @@
-function [pred] = stackedAEPredict(theta, inputSize, hiddenSize, numClasses, netconfig, data)
+function [pred] = stackedAEPredict_bak(theta, inputSize, hiddenSize, numClasses, netconfig, data)
                                          
 % stackedAEPredict: Takes a trained theta and a test data set,
 % and returns the predicted labels for each example.
@@ -23,22 +23,36 @@ stack = params2stack(theta(hiddenSize*numClasses+1:end), netconfig);
 %% ---------- YOUR CODE HERE --------------------------------------
 %  Instructions: Compute pred using theta assuming that the labels start 
 %                from 1.
-M = size(data,2);
-nStack = numel(stack);
-Z = cell(nStack+1,1);
-A = cell(nStack+1,1);
-A{1} = data;
-for d = 1:nStack
-	Z{d+1} = stack{d}.w * A{d} + repmat(stack{d}.b,1,M);
-	A{d+1} = sigmoid(Z{d+1});
+
+M = size(data, 2);
+
+%% feed forward
+
+% MLP
+numLayers = numel(stack);
+inputs = cell(numLayers+1,1);
+inputs{1} = data;
+for d = 1:numLayers
+    z = stack{d}.w * inputs{d} + repmat(stack{d}.b, [1,M]);
+    a = sigmoid(z);
+    inputs{d+1} = a;
 end
 
+% Softmax
+x = inputs{numLayers+1};
+z = softmaxTheta * x;
+ez = exp(z);
+ez_sum = sum(ez,1);
+h = ez ./ repmat(ez_sum,[numClasses,1]);
 
-MP = softmaxTheta*A{nStack+1};
-MP = bsxfun(@minus, MP, max(MP,[],1));
-MP = exp(MP);
-MP = bsxfun(@rdivide, MP, sum(MP,1));
-[mm pred] = max(MP,[],1);
+[test, pred] = max(h,[],1);
+
+
+
+
+
+
+
 
 
 % -----------------------------------------------------------
